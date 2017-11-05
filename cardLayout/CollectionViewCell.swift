9b852fc8e1b7d62093be787a33a3a89d764dc9b8
rileydnorris <rileydnorris@gmail.com>
2017-11-05 15:08:49 -0600

    Initial Commit

---
+++ cardLayout/CollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CollectionViewCell.swift
+//  cardLayout
+//
+//  Created by Riley Norris on 11/4/17.
+//  Copyright © 2017 Riley Norris. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var locationImage: UIImageView!
+    @IBOutlet weak var locationName: UILabel!
+    @IBOutlet weak var locationDescription: UILabel!
+    
+}
---
+++ cardLayout/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  cardLayout
+//
+//  Created by Riley Norris on 11/4/17.
+//  Copyright © 2017 Riley Norris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    let locationNames = ["Hawaii Resort", "Mountain Expedition", "Scuba Diving"]
+    
+    let locationImages = [UIImage(named: "hawaiiResort"), UIImage(named: "mountainExpedition"), UIImage(named: "scubaDiving")]
+    
+    let locationDescription = ["Beautiful resort off the coast of Hawaii", "Exhilarating mountainous expedition through Yosemite National Park", "Awesome Scuba Diving adventure in the Gulf of Mexico"]
+    
+    
+    
+    override func viewDidLoad() {
+    }
+
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        
+        cell.locationImage.image = locationImages[indexPath.row]
+        cell.locationName.text = locationNames[indexPath.row]
+        cell.locationDescription.text = locationDescription[indexPath.row]
+        
+        //This creates the shadows and modifies the cards a little bit
+        cell.contentView.layer.cornerRadius = 4.0
+        cell.contentView.layer.borderWidth = 1.0
+        cell.contentView.layer.borderColor = UIColor.clear.cgColor
+        cell.contentView.layer.masksToBounds = false
+        cell.layer.shadowColor = UIColor.gray.cgColor
+        cell.layer.shadowOffset = CGSize(width: 0, height: 1.0)
+        cell.layer.shadowRadius = 4.0
+        cell.layer.shadowOpacity = 1.0
+        cell.layer.masksToBounds = false
+        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
+        
+        
+        return cell
+    }
+
+}
+
